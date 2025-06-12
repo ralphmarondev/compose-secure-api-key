bd652af1ef7367465b448247167455485a4ddfe8
Ralph Maron Eda <edaralphmaron@gmail.com>
2025-06-12 23:40:51 +0800

    inject API_KEY from local.properties using BuildConfig

--- app/src/main/java/com/ralphmarondev/composesecureapikey/MainActivity.kt
+++ app/src/main/java/com/ralphmarondev/composesecureapikey/MainActivity.kt
@@ -4,6 +4,15 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.ralphmarondev.composesecureapikey.ui.theme.ComposeSecureApiKeyTheme
 
 class MainActivity : ComponentActivity() {
@@ -11,8 +20,30 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            ComposeSecureApiKeyTheme {
+            val apiKey = BuildConfig.API_KEY
+            val greetings = BuildConfig.GREETINGS
 
+            ComposeSecureApiKeyTheme {
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(16.dp),
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.Center
+                ) {
+                    Text(
+                        text = "$greetings Ralph Maron Eda is here!",
+                        fontSize = MaterialTheme.typography.titleLarge.fontSize,
+                        fontWeight = MaterialTheme.typography.titleLarge.fontWeight,
+                        color = MaterialTheme.colorScheme.primary
+                    )
+                    Text(
+                        text = "Api key: $apiKey",
+                        fontSize = MaterialTheme.typography.titleLarge.fontSize,
+                        fontWeight = MaterialTheme.typography.titleLarge.fontWeight,
+                        color = MaterialTheme.colorScheme.secondary
+                    )
+                }
             }
         }
     }
